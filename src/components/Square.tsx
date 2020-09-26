b7db10844b9d0f0cf884abc54f6c1865a454460a
Nikita Polyakov <nipolya1@mts.ru>
2020-09-26 18:38:57 +0300

    eject

---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Square.module.css';
+
+export default class Square extends React.Component {
+  render() {
+    return (
+      <button className={styles.square}>
+        {/* TODO */}
+      </button>
+    );
+  }
+}
