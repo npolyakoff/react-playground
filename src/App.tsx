9777a52d57bb8e903d058a871817ca9601d74b88
Nikita Polyakov <nipolya1@mts.ru>
2020-09-26 19:54:02 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import Board from './components/Board'
-// import AppHeader from 
+import AppHeader from '@Components/AppHeader/AppHeader';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+      <Router>
+        <div className="app pt-60">
+          <AppHeader />
+          <h1>Say meow</h1>
+          <Switch>
+            <Route path="/about">
+              about
+            </Route>
+            <Route path="/">
+              home
+            </Route>
+          </Switch>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,2 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import styles from './AppHeader.module.css';
 
-export default {}
+export default class AppHeader extends React.Component {
+  links: Array<{to: string, name: string}> = [{to: '/', name: 'Home'}, {to: '/about', name: 'About'}]
+  render() {
+    return (
+      <div className={`${styles.container} d-flex-center px-24`}>
+        <div className="mr-24">Meow Club</div>
+        <div>
+          { this.links.map(link => <Link to={link.to} className="mr-16">{link.name}</Link> ) }
+        </div>
+      </div>
+    )
+  }
+}
